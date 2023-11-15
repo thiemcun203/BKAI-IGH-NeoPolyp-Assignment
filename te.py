d0a09343c6bc725c32a6d910829cefe283abe513
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-11-15 19:19:54 +0700

    update infer.py

--- te.py
+++ te.py
@@ -5,48 +5,40 @@ import numpy as np
 import pandas as pd
 from albumentations.pytorch.transforms import ToTensorV2
 import albumentations as A
-import segmentation_models_pytorch as smp
-
+from final_polyp import PolypModel
+import argparse
+ 
 # Set device type
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = torch.device('mps' if torch.backends.mps.is_built() else 'cpu') 
 
 # Load the model
-model = smp.UnetPlusPlus(
-    encoder_name="resnet34",
-    encoder_weights="imagenet",
-    in_channels=3,
-    classes=3
-)
+model = PolypModel(3)
 model.to(device)
 
-parser = argparse.ArgumentParser(description='Polyp Segmentation')
+# Load the checkpoint
+parser = argparse.ArgumentParser(description='Polyp Segmentation Inference')
 parser.add_argument('--checkpoint', type=str, help='Path to the model checkpoint')
 parser.add_argument('--test_dir', type=str, help='Directory path to test images')
-parser.add_argument('--predict_dir', type=str, help='Directory path to save output masks')
 args = parser.parse_args()
-
-# Load the checkpoint
 checkpoint = torch.load(args.checkpoint, map_location=device)
 model.load_state_dict(checkpoint['model'])
 
 
-# Define the transformation for validation images
-normal_transform = A.Compose([
+val_transform = A.Compose([
     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2(),
 ])
 
-# Process test images
-trainsize = 256
-# trainsize = 512
+trainsize = 384
 color_dict= {0: (0, 0, 0),
              1: (255, 0, 0),
              2: (0, 255, 0)}
+
 def mask_to_rgb(mask, color_dict):
     output = np.zeros((mask.shape[0], mask.shape[1], 3))
     for k in color_dict.keys():
         output[mask==k] = color_dict[k]
+
     return np.uint8(output)    
 
 model.eval()
@@ -57,19 +49,17 @@ for i in os.listdir(args.test_dir):
     ori_w = ori_img.shape[0]
     ori_h = ori_img.shape[1]
     img = cv2.resize(ori_img, (trainsize, trainsize))
-    transformed = normal_transform(image=img)
+    transformed = val_transform(image=img)
     input_img = transformed["image"]
     input_img = input_img.unsqueeze(0).to(device)
     with torch.no_grad():
         output_mask = model.forward(input_img).squeeze(0).cpu().numpy().transpose(1,2,0)
     mask = cv2.resize(output_mask, (ori_h, ori_w))
     mask = np.argmax(mask, axis=2)
-    new_rgb_mask = np.zeros((*mask.shape, 3)).astype(np.uint8)
     mask_rgb = mask_to_rgb(mask, color_dict)
-    mask_rgb_true = cv2.cvtColor(mask_rgb, cv2.COLOR_BGR2RGB)
     mask_rgb = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2BGR)
-    cv2.imwrite(args.predict_dir.format(i), mask_rgb)
-print("Loaded & Processed Test file")
+    cv2.imwrite("predicted_mask/{}".format(i), mask_rgb)
+    print('Predicted mask saved')
 
 def rle_to_string(runs):
     return ' '.join(str(x) for x in runs)
@@ -130,13 +120,13 @@ def mask2string(dir):
     }
     return r
 
-MASK_DIR_PATH = args.predict_dir 
+
+MASK_DIR_PATH = '/kaggle/working/predicted_mask' 
 dir = MASK_DIR_PATH
 res = mask2string(dir)
 df = pd.DataFrame(columns=['Id', 'Expected'])
 df['Id'] = res['ids']
 df['Expected'] = res['strings']
 
-df.to_csv(r'submission.csv', index=False)
-print("Done")
-
+df.to_csv(r'dinzdzunOutput.csv', index=False)
+print("Saved Output")
